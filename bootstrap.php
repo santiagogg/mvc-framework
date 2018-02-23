3ab473ea2aae2e5a585ac49a859db3b79afad6a3
santiagogg <santiagogg@users.noreply.github.com>
2018-02-22 23:01:48 -0300

    add vlucas/phpdotenv to load environment variables from .env file
    Note: Copy  .env.example  to .env

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,9 @@
 // Load our autoloader
 require_once __DIR__.'/vendor/autoload.php';
 
+//
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
 //Instantiate our Container
 $container = new League\Container\Container;
