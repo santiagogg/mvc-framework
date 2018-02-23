dc675ec0c78f4c2f023778b12eb7429029e5f542
santiagogg <santiagogg@users.noreply.github.com>
2018-02-23 16:46:04 -0300

    Add PDO Mysql connection

--- app/controllers/MoviesController.php
+++ app/controllers/MoviesController.php
@@ -10,34 +10,52 @@
 namespace Controllers;
 
 use Models\Movies;
+use PDO;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Twig_Environment;
 
 class MoviesController extends BaseController
 {
+    protected $conn;
+    
+    /**
+     * MoviesController constructor.
+     *
+     * @param \Twig_Environment $view
+     * @param \PDO $conn
+     */
+    public function __construct(Twig_Environment $view, PDO $conn) {
+        parent::__construct($view);
+        $this->conn = $conn;
+    }
+    
     public function index(ServerRequestInterface $request, ResponseInterface $response) {
-        
         $movies = new Movies();
-        
         return $this->view($response, 'movies/index.twig', ['movies' => $movies->onShow()]);
     }
     
     public function vote(ServerRequestInterface $request, ResponseInterface $response, array $args) {
         
         $movieId = $args['id'];
-        //TODO:persist data in db
-        $vote = [
-            'movie_id' => $movieId,
-            'voted_at' => date('Y-m-d H:i:s')
-        ];
+        $time_stamp = date('Y-m-d H:i:s');
         
+        //persist data in db
+        $sql = "INSERT INTO votes (movie_id, voted_at) VALUES (:movie_id, :voted_at)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            ':movie_id' => $movieId,
+            ':voted_at' => $time_stamp
+        ]);
         
-        //TODO:get the total votes from db
-        $movieUpdated = [
-            'movie_id'    => $movieId,
-            'last_voted'  => $vote['voted_at'],
-            'total_votes' => 1
-        ];
+        //get the total_votes / last_votes from db
+        $sql = "select movie_id, count(*) total_votes, MAX(voted_at) last_voted from votes where movie_id = :movie_id group by movie_id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            ':movie_id' => $movieId
+        ]);
+        $movieUpdated = $stmt->fetch();
+        
         
         $response->getBody()->write(json_encode($movieUpdated));
         
--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load our autoloader
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Use Dotenv to load environment variables from .env file
 $dotenv = new Dotenv\Dotenv(__DIR__);
@@ -10,26 +10,47 @@ $dotenv->load();
 //Instantiate our Container
 $container = new League\Container\Container;
 
+//Response
 $container->share('response', Zend\Diactoros\Response::class);
-$container->share('request', function () {
-    return Zend\Diactoros\ServerRequestFactory::fromGlobals(
-        $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
-    );
+
+//Request
+$container->share('request', function() {
+    return Zend\Diactoros\ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 });
 
+//Emiter
 $container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
 
-$container->share('Twig', function () {
+// Template Engine
+$container->share('Twig', function() {
     // Specify our Twig templates location
-    $twigLoader = new Twig_Loader_Filesystem(__DIR__.'/app/views');
+    $twigLoader = new Twig_Loader_Filesystem(__DIR__ . '/app/views');
     
     return new Twig_Environment($twigLoader);
 });
 
+//PDO Connection
+$container->share('PDO', function() {
+    try {
+        $host = getenv('DB_HOST');
+        $database = getenv('DB_DATABASE');
+        $username = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
+        $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        $conn = null;
+    }
+    
+    return $conn;
+});
+
 //Inject dependencies to controller
 $container->add(\Controllers\MoviesController::class)
-    ->withArgument($container->get('Twig'));
-
+    ->withArgument($container->get('Twig'))
+    ->withArgument($container->get('PDO'));
 
 //Instantiate our Router
 $route = new League\Route\RouteCollection($container);
